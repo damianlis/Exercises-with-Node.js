c1b3d62bd2eeb598e3cb123adce0c0d549140fd3
damianlis <lis.damian@yahoo.pl>
2018-07-08 12:49:33 +0200

    finished

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,13 @@
-process.argv.forEach(arg,)
+//console.log(process.argv[2]);
+
+const fs = require('fs');
+const crypto = require('crypto');
+
+fs.readFile(process.argv[2], 'utf8', (err,data)=>{
+    if( err === null ) {
+        const createHash = crypto.createHmac('sha256',data).digest('hex');
+        console.log('Oto funkcja skrótu: ', createHash);
+    } else {
+        console.log('Nie bangla');
+    }
+})
